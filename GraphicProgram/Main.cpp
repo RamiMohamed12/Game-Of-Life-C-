6239c7013c6b8ecb0a29ad0b631d9a4392b168df
10caaa <SidAli.HALLAOUA.dz@viacesi.fr>
2024-12-05 21:24:06 +0100

    Update Main.cpp

--- GraphicProgram/Main.cpp
+++ GraphicProgram/Main.cpp
@@ -1,8 +1,14 @@
-#include <SFML/Graphics.hpp>
 #include "Game.h"
+#include <iostream>
 
 int main() {
-    Game game;
-    game.run();
+    try {
+        Game game;
+        game.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
