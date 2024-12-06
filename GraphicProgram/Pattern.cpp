267c351cb48d2cfe46d2882206b4606daff94f14
10caaa <SidAli.HALLAOUA.dz@viacesi.fr>
2024-12-06 18:21:39 +0100

    Update Pattern.cpp

--- GraphicProgram/Pattern.cpp
+++ GraphicProgram/Pattern.cpp
@@ -10,13 +10,29 @@ std::map<std::string, std::vector<std::vector<bool>>> Pattern::getPatterns() {
         {"Small Exploder", {
             {0, 1, 0},
             {1, 1, 1},
-            {1, 0, 1}
+            {1, 0, 1},
+            {0, 1, 0}
         }},
         {"Spaceship", {
             {0, 1, 1, 1, 0},
             {1, 0, 0, 0, 1},
             {0, 0, 0, 0, 1},
             {1, 0, 0, 1, 0}
+        }},
+        {"Pulsar", {
+            {0,0,1,1,1,0,0,0,1,1,1,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {1,0,0,0,0,1,0,1,0,0,0,0,1},
+            {1,0,0,0,0,1,0,1,0,0,0,0,1},
+            {1,0,0,0,0,1,0,1,0,0,0,0,1},
+            {0,0,1,1,1,0,0,0,1,1,1,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,1,1,1,0,0,0,1,1,1,0,0},
+            {1,0,0,0,0,1,0,1,0,0,0,0,1},
+            {1,0,0,0,0,1,0,1,0,0,0,0,1},
+            {1,0,0,0,0,1,0,1,0,0,0,0,1},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,1,1,1,0,0,0,1,1,1,0,0}
         }}
     };
 }
@@ -24,7 +40,11 @@ std::map<std::string, std::vector<std::vector<bool>>> Pattern::getPatterns() {
 void Pattern::insertPattern(Grid& grid, const std::vector<std::vector<bool>>& pattern, int startX, int startY) {
     for (size_t y = 0; y < pattern.size(); ++y) {
         for (size_t x = 0; x < pattern[y].size(); ++x) {
-            grid.setCell(startX + x, startY + y, pattern[y][x]);
+            int gridX = startX + x;
+            int gridY = startY + y;
+            if (gridX >= 0 && gridX < grid.getWidth() && gridY >= 0 && gridY < grid.getHeight()) {
+                grid.setCell(gridX, gridY, pattern[y][x]);
+            }
         }
     }
 }
