0ce77cdeb5cd4f55288dad20ad6060129e1dae30
RamiMohamed12 <roomierami00@gmail.com>
2024-12-07 21:39:55 +0100

    removed Copy

--- Copy/DeadCell.cpp
+++
@@ -1,9 +0,0 @@
-#include "DeadCell.h"
-#include <SFML/Graphics.hpp>
-
-void DeadCell::draw(sf::RenderWindow& window, float x, float y, float size) const {
-    sf::RectangleShape cell(sf::Vector2f(size - 1, size - 1));
-    cell.setPosition(x, y);
-    cell.setFillColor(sf::Color(30, 30, 40));  // Dark background for dead cells
-    window.draw(cell);
-}
